619c09497d690079f3570091c512c548fd978d79
hrafn65 <hrafn65@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2009-08-07 12:26:29 +0000

    Initial release of IceNLP
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@44 ea662450-206c-40bb-b37a-5b5cf4d83bb5

---
+++ is/iclt/icenlp/core/icetagger/IceTaggerOutputApertium.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2009 Hrafn Loftsson
+ *
+ * This file is part of the IceNLP toolkit.
+ * IceNLP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * IceNLP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with IceNLP. If not,  see <http://www.gnu.org/licenses/>.
+ *
+ * Contact information:
+ * Hrafn Loftsson, School of Computer Science, Reykjavik University.
+ * hrafn@ru.is
+ */
+package is.iclt.icenlp.core.icetagger;
+
+import is.iclt.icenlp.core.tokenizer.IceTokenTags;
+
+import java.io.IOException;
+
+/**
+ * Generates the output for IceTagger in Apertium format.
+ * @author Hrafn Loftsson
+ */
+public class IceTaggerOutputApertium extends IceTaggerOutput{
+
+    public IceTaggerOutputApertium(int outFormat, String wordTagSeparator, boolean useFullOutput, boolean useFullDisambiguation, String tagMapFile, String lemmatizerFile) throws IOException
+    {
+        super(outFormat, wordTagSeparator, useFullOutput, useFullDisambiguation, tagMapFile, lemmatizerFile);
+    }
+
+    public String buildOutput( IceTokenTags tok, int index, int numTokens )
+    {
+        String str, tag, mappedTag;
+        String lemma="";
+
+        tag = tok.getFirstTagStr();
+
+        if (myTagMap != null)
+            mappedTag = getMappedTag(tag);
+        else
+            mappedTag = tag;
+
+        // Add the lemma?
+        if (myLemmald != null)
+            lemma = myLemmald.getLemma(tok.lexeme, tag);
+
+        //str = "^" + tok.lexeme + "/" + lemma + mappedTag + "$ ";
+        // Apertium þarf ekki orðið sjálft, eingöngu lemmuna
+        str = "^" + lemma + mappedTag + "$ ";
+        return str;
+    }
+}
