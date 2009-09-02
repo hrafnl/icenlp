ea2e2d00423e8f25c0bb0855f4a550ceaacc529f
antonkarl <antonkarl@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2009-09-02 15:58:06 +0000

    :-)
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@522 ea662450-206c-40bb-b37a-5b5cf4d83bb5

--- flex/icetagger/compileRules.sh
+++ flex/icetagger/compileRules.sh
@@ -1,1 +1,4 @@
-java genIceRules IceRules.rul > IceLocalRules.java
+jflex genIceRules.flex
+javac genIceRules.java
+java genIceRules IceRules.rul > ../../src/is/iclt/icenlp/core/icetagger/IceLocalRules.java
+
