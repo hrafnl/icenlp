b8c0dab8d35f0f84961086c5cfe2ce879d12591d
hlynurs <hlynurs@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2009-11-01 14:56:50 +0000

    Clean up
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@564 ea662450-206c-40bb-b37a-5b5cf4d83bb5

--- tools/IceNLPServer/src/is/ru/icenlpserver/common/Pair.java
+++ tools/IceNLPServer/src/is/ru/icenlpserver/common/Pair.java
@@ -1,10 +1,19 @@
 package is.ru.icenlpserver.common;
 
+/**
+ * Generic pair class.
+ * @author hlynurs
+ *
+ * @param <l> Type of the left object.
+ * @param <r> Type of the right object.
+ */
 public class Pair<l, r>
 {
+	// Member variables.
 	public l one;
 	public r two;
 	
+	// Constructor for the class. 
 	public Pair(l one, r two)
 	{
 		this.one = one;
--- tools/IceNLPServer/src/is/ru/icenlpserver/common/Word.java
+++ tools/IceNLPServer/src/is/ru/icenlpserver/common/Word.java
@@ -1,34 +1,78 @@
 package is.ru.icenlpserver.common;
 
+/**
+ * Represents word within a sentence and keps
+ * information about it: Lexeme, part-of-speech tag
+ * and the lemma.
+ * @author hlynurs
+ */
 public class Word 
 {
+	// Member variables.
 	private String lexeme;
 	private String tag;
 	private String lemma;
 	
+	// Constructor for the class.
 	public Word(String lexeme, String tag) 
 	{
 		this.lexeme = lexeme;
 		this.tag = tag;
 	}
-	public String getLexeme() {
+	
+	/**
+	 * Getter function for the lexeme member variable.
+	 * @return String object that contains the lexeme of
+	 * of the word.
+	 */
+	public String getLexeme() 
+	{
 		return lexeme;
 	}
-	public void setLexeme(String lexeme) {
+	
+	/**
+	 * Setter function for the lexeme member variable.
+	 * @param lexeme String object that contains the 
+	 * lexeme of the word.
+	 */
+	public void setLexeme(String lexeme) 
+	{
 		this.lexeme = lexeme;
 	}
-	public String getTag() {
+	
+	/**
+	 * Getter function for the tag member variable.
+	 * @return String object that contains the tag.
+	 */
+	public String getTag() 
+	{
 		return tag;
 	}
-	public void setTag(String tag) {
+	
+	/**
+	 * Setter function for the tag member variable.
+	 * @param tag String object that contains the tag.
+	 */
+	public void setTag(String tag) 
+	{
 		this.tag = tag;
 	}
-	public String getLemma() {
+	
+	/**
+	 * Getter function for the lemma member variable.
+	 * @return String object that contains the lemma.
+	 */
+	public String getLemma() 
+	{
 		return lemma;
 	}
-	public void setLemma(String lemma) {
+	
+	/**
+	 * Setter function fot the lemma member variable.
+	 * @param lemma String object that contains the lemma.
+	 */
+	public void setLemma(String lemma) 
+	{
 		this.lemma = lemma;
 	}
-	
-	
 }
