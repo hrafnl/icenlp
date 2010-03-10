973dfa1681e815d87054f97d4daa38dddda43d33
hloftsson <hloftsson@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2010-03-10 00:09:36 +0000

    One local rule added and changes to mapping
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@653 ea662450-206c-40bb-b37a-5b5cf4d83bb5

--- bat/icetagger/icetaggerApertium.sh
+++ bat/icetagger/icetaggerApertium.sh
@@ -1,1 +1,1 @@
-java -Xmx256M -classpath ../../dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceTaggerApertium -tm ../../dict/icetagger/otb.apertium.dict -lem $1 $2 $3 $4 $5 $6
+java -Xmx256M -classpath ../../dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceTaggerApertium -tm ../../dict/icetagger/otb.apertium.dict -sf $1 $2 $3 $4 $5 $6
