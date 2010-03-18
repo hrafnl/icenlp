054003d869f4ba2b2f9c8b6585d8c46638fa5327
hloftsson <hloftsson@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2010-03-18 15:07:10 +0000

    Memory size increased
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@694 ea662450-206c-40bb-b37a-5b5cf4d83bb5

--- server/sh/RunServer.sh
+++ server/sh/RunServer.sh
@@ -1,4 +1,4 @@
 #\!/bin/sh
 cd `dirname $0`
-java -classpath ../dist/IceNLPServer.jar is.ru.iclt.icenlp.server.runner.Runner --config=../configs/server.conf
+java -Xms64m -Xmx512m -classpath ../dist/IceNLPServer.jar is.ru.iclt.icenlp.server.runner.Runner --config=../configs/server.conf
 		
