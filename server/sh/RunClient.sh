9c1fc5d8845bc0e499b1e8645dfb6ff84532910a
hlynurs <hlynurs@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2010-04-18 17:50:25 +0000

    package changed for client
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@712 ea662450-206c-40bb-b37a-5b5cf4d83bb5

--- server/sh/RunClient.sh
+++ server/sh/RunClient.sh
@@ -1,4 +1,4 @@
 #\!/bin/sh
 cd `dirname $0`
-java -classpath ../dist/IceNLPServer.jar is.ru.iclt.icenlp.client.runner.Runner --port=1234 --host=localhost
+java -classpath ../dist/IceNLPServer.jar is.iclt.icenlp.client.runner.Runner --port=1234 --host=localhost
 		
