32e6d74ae75d63be6d88c01e94684944b62c907e
hlynurs <hlynurs@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2010-04-18 14:02:22 +0000

    Router is now using a configuration file instead of using parameters.
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@706 ea662450-206c-40bb-b37a-5b5cf4d83bb5

--- server/sh/RunRouter.sh
+++ server/sh/RunRouter.sh
@@ -1,4 +1,3 @@
 #\!/bin/sh
 cd `dirname $0`
-java -Xms64m -Xmx512m -Dfile.encoding=UTF-8 -classpath ../dist/IceNLPServer.jar is.iclt.icenlp.router.runner.Runner -h=$1
-		
+java -Xms64m -Xmx512m -Dfile.encoding=UTF-8 -classpath ../dist/IceNLPServer.jar is.iclt.icenlp.router.runner.Runner ../configs/router.conf
--- server/src/is/iclt/icenlp/router/runner/Runner.java
+++ server/src/is/iclt/icenlp/router/runner/Runner.java
@@ -1,9 +1,12 @@
 package is.iclt.icenlp.router.runner;
 
+import is.iclt.icenlp.common.configuration.Configuration;
 import is.iclt.icenlp.router.threads.RequestListneningThread;
 import is.iclt.icenlp.router.threads.RequestListneningThreadException;
 import is.iclt.icenlp.router.threads.SlaveListeningThread;
 import is.iclt.icenlp.router.threads.SlaveListeningThreadException;
+
+import java.io.File;
 
 /**
  * Runner class for the router.
@@ -13,74 +16,67 @@ public class Runner {
 	public static String prefix = "[RouterRunner]: ";
 
 	public static void main(String[] args) {
-		System.out.println();
-		System.out.println(">> IceNLP Router");
+		System.out.println("- IceNLP router");
+        
+        if(args.length >= 2){
+            System.out.println(prefix + "error in parameters.");
+            System.out.println(prefix + "stopping.");
+            help();
+            return;
+        }
 
-		// The hostname for the router server.
+        else if(args.length == 1){
+
+            if(args[0].equals("--help")){
+                help();
+                return;
+            }
+
+            String configFileLocation = args[0];
+            File f = new File(configFileLocation);
+            if(f.exists() && f.canRead()){
+                System.out.println(prefix + "reading configuration file from: " + configFileLocation);
+                Configuration.loadConfig(args[0]);
+            }
+            else{
+                System.out.println(prefix + "unable to read configuration file from: " + configFileLocation);
+                System.out.println(prefix + "stopping.");
+                return;
+            }
+        }
+
+        else{
+            File f = new File("router.conf");
+            System.out.println(prefix + "Reading default configuration file: " + f.getAbsolutePath());
+            if(f.exists() && f.canRead())
+                Configuration.loadConfig("router.conf");
+            else{
+                System.out.println(prefix + "unable to read default configuration file: " + f.getAbsolutePath());
+                System.out.println(prefix + "stopping.");
+                return;
+            }
+        }
+
+        // The hostname for the router server.
 		// default hostname is set to "localhost".
-		String hostname = "localhost";
+		String hostName = "localhost";
 
 		// The port that the server listens to for
 		// incoming slaves. Default port is 2525.
-		String slavePort = "2525";
+		String slavePort = Configuration.getInstance().getValue("slaveport");
 
 		// The port that the server listens to for
 		// incoming translation requests. Default port
 		// is 2526.
-		String requestport = "2526";
-
-		// let's look through the arguments that
-		// are passed by the user.
-		/*
-		for (String arg : args) {
-			// argument check for --slaveport (-s)
-			if (arg.matches("-slaveport=[0-9]+|-s=[0-9]+")) {
-				if (arg.startsWith("-slaveport="))
-					arg = arg.replace("-slaveport=", "");
-				else
-					arg = arg.replace("-s=", "");
-
-				slavePort = arg;
-			}
-
-			// argument check for --request-port (-r)
-			else if (arg.matches("-request-port=[0-9]+|-r=[0-9]+")) {
-				if (arg.startsWith("-request-port="))
-					arg = arg.replace("-request-port=", "");
-				else
-					arg = arg.replace("-r=", "");
-
-				requestport = arg;
-			}
-
-			// argument check for --host (-h)
-			else if (arg.matches("-host=[a-zA-Z0-9.]+|-h=[a-zA-Z0-9.]+")) {
-				if (arg.startsWith("-host="))
-					arg = arg.replace("-host=", "");
-				else
-					arg = arg.replace("-h=", "");
-
-				hostname = arg;
-			} else if (arg.matches("-help")) {
-				help();
-				return;
-			}
-
-			else {
-				help();
-				System.out.println(prefix + "unknown parameter " + arg);
-				return;
-			}
-		}
-		*/
-
+		String requestPort = Configuration.getInstance().getValue("requestport");
+        
 		// Let's display all the settings.
-		System.out.println(prefix + "hostname set to " + hostname);
+		System.out.println(prefix + "hostname set to " + Configuration.getInstance().getValue("hostname"));
 
 		// Let's start the slave listening thread.
 		SlaveListeningThread slaveListeningThread = null;
 		try {
-			slaveListeningThread = new SlaveListeningThread(hostname, slavePort);
+			slaveListeningThread = new SlaveListeningThread(hostName, slavePort);
 		} catch (SlaveListeningThreadException e) {
 			System.out.println(prefix
 					+ "unable to start Slave Listening thread: "
@@ -93,7 +89,7 @@ public class Runner {
 		// Let's start the request listening thread.
 		RequestListneningThread requestThread = null;
 		try {
-			requestThread = new RequestListneningThread(hostname, requestport);
+			requestThread = new RequestListneningThread(hostName, requestPort);
 		} catch (RequestListneningThreadException e) {
 			System.out.println(prefix
 					+ "unable to start Request Listening thread: "
@@ -106,9 +102,7 @@ public class Runner {
 
 	public static void help() {
 		System.out.println("parameters:");
-		System.out.println("-slaveport|-s         The port that the incoming slaves connects to.");
-		System.out.println("-request-port|-r      The port that clients use to make translation requests.");
-		System.out.println("-host|-h              Hostname for the server. Used to bind the socket.");
-		System.out.println("-help                 Prints this menu.");
+		System.out.println("[configuration file]      Path to configuration file to load.");
+		System.out.println("--help                    Prints this menu.");
 	}
 }
