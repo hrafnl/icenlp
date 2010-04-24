f7c64e5ac502f85840c0903b47b66a64313f7656
hlynurs <hlynurs@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2010-04-24 18:05:07 +0000

    code cleanup.
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@726 ea662450-206c-40bb-b37a-5b5cf4d83bb5

--- server/src/is/iclt/icenlp/client/network/ClientNetworkHandler.java
+++ server/src/is/iclt/icenlp/client/network/ClientNetworkHandler.java
@@ -10,144 +10,125 @@ import java.net.Socket;
 import java.util.LinkedList;
 import java.util.List;
 
-/***
- *  ClientNetworkHandler handles all the network communication from the
- *  client side to the server.
+/**
+ * ClientNetworkHandler handles all the network communication from the
+ * client side to the server.
  */
-public class ClientNetworkHandler
-{
+public class ClientNetworkHandler {
     private String host;
     private String port;
 
-    public ClientNetworkHandler(String host, String port){
+    public ClientNetworkHandler(String host, String port) {
         this.host = host;
         this.port = port;
     }
-    
-    public String tagString(String lines) throws IOException
-	{
-		Socket socket = new Socket(this.host, Integer.parseInt(this.port));
-        InputStream inStream = socket.getInputStream();
-		OutputStream outStream = socket.getOutputStream();
-        
-		createSentecePackets(outStream, lines);
-		Packet p = readFromStream(inStream);
-		if(p.getOpcode() == 3)
-		{
-			int size = 0;
-			int numOfPacks = is.iclt.icenlp.common.network.ByteConverter.bytesToInt(p.getData(), 4);
-			byte[] dataFromServer = new byte[numOfPacks * 512];
-			int byteCounter = 0;
 
-			for(int i = 0; i<numOfPacks; i++)
-			{
+    public String tagString(String lines) throws IOException {
+        Socket socket = new Socket(this.host, Integer.parseInt(this.port));
+        InputStream inStream = socket.getInputStream();
+        OutputStream outStream = socket.getOutputStream();
+
+        createSentecePackets(outStream, lines);
+        Packet p = readFromStream(inStream);
+        if (p.getOpcode() == 3) {
+            int size = 0;
+            int numOfPacks = is.iclt.icenlp.common.network.ByteConverter.bytesToInt(p.getData(), 4);
+            byte[] dataFromServer = new byte[numOfPacks * 512];
+            int byteCounter = 0;
+
+            for (int i = 0; i < numOfPacks; i++) {
                 p = readFromStream(inStream);
-				if(p.getOpcode() != 4){
-                    System.out.println("[error]: Invalid opcode from server, expected 4, got " + p.getOpcode());   
+                if (p.getOpcode() != 4) {
+                    System.out.println("[error]: Invalid opcode from server, expected 4, got " + p.getOpcode());
                     socket.close();
                     return "";
                 }
                 int dataSize = is.iclt.icenlp.common.network.ByteConverter.bytesToInt(p.getData(), 4);
-				size = size + dataSize;
-				for(int j = 0; j<dataSize; j++)
-				{
-					dataFromServer[byteCounter++] = p.getData()[j+8];
-				}
-			}
+                size = size + dataSize;
+                for (int j = 0; j < dataSize; j++) {
+                    dataFromServer[byteCounter++] = p.getData()[j + 8];
+                }
+            }
 
-			String strServer = new String(dataFromServer, 0, size);
-			socket.close();
+            String strServer = new String(dataFromServer, 0, size);
+            socket.close();
             return strServer;
-		}
-		else
-		{
-			System.out.println("[error]: Invalid opcode from server, expected 3, got " + p.getOpcode());
-			return "";
-		}
-	}
+        } else {
+            System.out.println("[error]: Invalid opcode from server, expected 3, got " + p.getOpcode());
+            return "";
+        }
+    }
 
 
-	private Packet readFromStream(InputStream stream)
-	{
-		byte[] data = new byte[512];
-		Packet packet = null;
-		try
-		{
-			int readSize = stream.read(data, 0, 512);
-			packet = new Packet(data, readSize);
-		}
+    private Packet readFromStream(InputStream stream) {
+        byte[] data = new byte[512];
+        Packet packet = null;
+        try {
+            int readSize = stream.read(data, 0, 512);
+            packet = new Packet(data, readSize);
+        }
 
-		catch (IOException e)
-		{
-			System.out.println("error: " + e.getMessage());
-		}
+        catch (IOException e) {
+            System.out.println("[error]: " + e.getMessage());
+        }
 
-		return packet;
-	}
+        return packet;
+    }
 
-	private void createSentecePackets(OutputStream stream, String sentence)
-	{
-		try
-		{
-			// list of packets that we will send.
-			List<Packet> packets = new LinkedList<Packet>();
+    private void createSentecePackets(OutputStream stream, String sentence) {
+        try {
+            // list of packets that we will send.
+            List<Packet> packets = new LinkedList<Packet>();
 
-			byte[] strBytes = sentence.getBytes("UTF8");
-			int numberOfpackets = ((int) Math.floor((strBytes.length/504)))+1;
+            byte[] strBytes = sentence.getBytes("UTF8");
+            int numberOfpackets = ((int) Math.floor((strBytes.length / 504))) + 1;
 
-			// Let's create the packet that we will send first
-			byte[] opcodeData = ByteConverter.itToByte(1);
-			byte[] sizeData = ByteConverter.itToByte(numberOfpackets);
-			byte[] data = new byte[512];
-			int p1counter = 0;
+            // Let's create the packet that we will send first
+            byte[] opcodeData = ByteConverter.itToByte(1);
+            byte[] sizeData = ByteConverter.itToByte(numberOfpackets);
+            byte[] data = new byte[512];
+            int p1counter = 0;
 
-			for(int i = 0; i<4; i++)
-			{
-				data[p1counter] = opcodeData[i];
-				p1counter++;
-			}
+            for (int i = 0; i < 4; i++) {
+                data[p1counter] = opcodeData[i];
+                p1counter++;
+            }
 
-			for(int i=0; i<4; i++)
-			{
-				data[p1counter] = sizeData[i];
-				p1counter++;
-			}
+            for (int i = 0; i < 4; i++) {
+                data[p1counter] = sizeData[i];
+                p1counter++;
+            }
 
-			packets.add(new Packet(data));
+            packets.add(new Packet(data));
 
 
-			int dataByteCounter = 0;
-			for(int i = 0; i<numberOfpackets; i++)
-			{
-				byte[] packetData = new byte[512];
-				int dataSize = 0;
+            int dataByteCounter = 0;
+            for (int i = 0; i < numberOfpackets; i++) {
+                byte[] packetData = new byte[512];
+                int dataSize = 0;
 
-				for(int j = 0; j<504 && dataByteCounter<strBytes.length; j++)
-				{
-					packetData[j+8] = strBytes[dataByteCounter];
-					dataByteCounter++;
-					dataSize++;
-				}
+                for (int j = 0; j < 504 && dataByteCounter < strBytes.length; j++) {
+                    packetData[j + 8] = strBytes[dataByteCounter];
+                    dataByteCounter++;
+                    dataSize++;
+                }
 
-				opcodeData = ByteConverter.itToByte(2);
-				sizeData = ByteConverter.itToByte(dataSize);
-				for(int j=0; j<4; j++)
-				{
-					packetData[j] = opcodeData[j];
-					packetData[j+4] = sizeData[j];
-				}
-				packets.add(new Packet(packetData));
+                opcodeData = ByteConverter.itToByte(2);
+                sizeData = ByteConverter.itToByte(dataSize);
+                for (int j = 0; j < 4; j++) {
+                    packetData[j] = opcodeData[j];
+                    packetData[j + 4] = sizeData[j];
+                }
+                packets.add(new Packet(packetData));
 
-			}
-			for(Packet pack: packets)
-			{
-				stream.write(pack.getData());
-			}
-		}
+            }
+            for (Packet pack : packets) {
+                stream.write(pack.getData());
+            }
+        }
 
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}		
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
