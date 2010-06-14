c2ff2a80f8000d0ecd41dcb2e107fe7a84f243dd
hloftsson <hloftsson@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2010-06-14 14:28:45 +0000

    IceParser shell scripts harmonised and documentation updated
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@759 ea662450-206c-40bb-b37a-5b5cf4d83bb5

--- core/bat/iceparser/iceparser.sh
+++ core/bat/iceparser/iceparser.sh
@@ -1,1 +1,1 @@
-java -classpath ../../dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceParser $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -classpath ../../dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceParser -i $1 -o $2 $3 $4 $5 $6 $7 $8 $9
--- core/bat/iceparser/iceparserOut.sh
+++ core/bat/iceparser/iceparserOut.sh
@@ -1,1 +1,1 @@
-java -classpath ../../dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceParserOut -i $1 -o $2 -p $3 $4 $5 $6
+java -classpath ../../dist/IceNLPCore.jar is.iclt.icenlp.runner.RunIceParserOut -i $1 -o $2 -p $3 $4 $5 $6 $7 $8 $9
