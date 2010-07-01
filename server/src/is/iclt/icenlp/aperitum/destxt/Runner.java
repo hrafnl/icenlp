0c10fc9aec0f46c8dcec586f4ba691241d29d56c
hlynurs <hlynurs@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2010-07-01 20:30:37 +0000

    Added runner new app destxt, which will be used to correct the format of the apertium output before it will be sent to the apertium pipeline.
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@771 ea662450-206c-40bb-b37a-5b5cf4d83bb5

---
+++ server/src/is/iclt/icenlp/aperitum/destxt/Runner.java
@@ -0,0 +1,32 @@
+package is.iclt.icenlp.aperitum.destxt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			String str = "";
+			str = in.readLine();
+			while (str != null) {
+				System.out.println(processLine(str));
+				str = in.readLine();	
+			}
+		} 
+		catch (IOException e) {
+			System.out.println("error> " +e.getMessage());
+			System.exit(0);
+		}
+	}
+	
+	public static String processLine(String line) {
+		line = line.replace("^</<$", "\\<");
+		line = line.replace("^>/>$", "\\>");
+		line = line.replace("^{/{$", "\\{");
+		//line = line.replace("$", "");
+		return line;
+	}
+}
