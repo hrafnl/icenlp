3aeb7124ce7350ba5760983ebb2c9169485ceadb
hlynurs <hlynurs@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2010-07-02 14:58:18 +0000

    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@775 ea662450-206c-40bb-b37a-5b5cf4d83bb5

--- server/src/is/iclt/icenlp/aperitum/destxt/Runner.java
+++ server/src/is/iclt/icenlp/aperitum/destxt/Runner.java
@@ -1,8 +1,13 @@
 package is.iclt.icenlp.aperitum.destxt;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Runner {
 
@@ -23,21 +28,30 @@ public class Runner {
 	}
 	
 	public static String processLine(String line) {
-		line = line.replace("^</<$", "\\<");
-		line = line.replace("^>/>$", "\\>");
-		line = line.replace("^{/{$", "\\{");
-		line = line.replace("^}/}$", "\\}");
-		line = line.replace("^[/[$", "\\[");
-		line = line.replace("^]/]$", "\\]");
+		line = line.replace("^$/$$", "\\$");
 		line = line.replace(",/,<cm>", ",<cm>");
 		line = line.replace("./.<sent>", ".<sent>");
 		line = line.replace("?/?<sent>", "?<sent>");
 		line = line.replace("!/!<sent>", "!<sent>");
-		line = line.replace("^(/($", "(");
-		line = line.replace("^)/)$", ")");
-		//line = line.replace("$", "");
-		
-		
+		line = line.replace("^[/[$", "\\[");
+		line = line.replace("^]/]$", "\\]");
+		line = line.replace("^{/{$", "\\{");
+		line = line.replace("^}/}$", "\\}");
+		Pattern pattern = Pattern.compile("(\\^./.[<[a-zA-Z0-9]+>]?\\$)");
+		Matcher matcher = pattern.matcher(line);
+		List<String> changes = new LinkedList<String>();
+		while(matcher.find()){
+			int groupcount = matcher.groupCount();
+			for (int i = 1; i <= groupcount; i++) {
+				String m = line.substring(matcher.start(i),matcher.end(i));
+				String[] msplit = m.split("/");
+				if(msplit[0].charAt(1) == msplit[1].charAt(0))
+					changes.add(m);				
+			}
+		}
+		for(String s : changes){
+			line = line.replace(s, ""+ s.charAt(1));
+		}
 		return line;
 	}
 }
