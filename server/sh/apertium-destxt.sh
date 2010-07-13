38b8da13e8a5cfdd2d7524db5a8c64068a43d57a
hlynurs <hlynurs@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2010-07-13 21:18:03 +0000

    error in sh.
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@780 ea662450-206c-40bb-b37a-5b5cf4d83bb5

--- server/sh/apertium-destxt.sh
+++ server/sh/apertium-destxt.sh
@@ -1,4 +1,4 @@
 #\!/bin/sh
 cd `dirname $0`
-java -classpath ../dist/IceNLPServer.jar is.iclt.icenlp.aperitum.destxt.Runner
+java -classpath ../dist/IceNLPServer.jar is.iclt.icenlp.apertium.destxt.Runner
 		
