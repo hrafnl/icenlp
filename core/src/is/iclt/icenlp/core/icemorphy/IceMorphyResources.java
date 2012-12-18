595bba880c154426066705cc053dbd33c4057dd4
hloftsson <hloftsson@ea662450-206c-40bb-b37a-5b5cf4d83bb5>
2012-12-18 14:08:39 +0000

    Added IceMorphyResources.java
    git-svn-id: svn://svn.code.sf.net/p/icenlp/code@902 ea662450-206c-40bb-b37a-5b5cf4d83bb5

---
+++ core/src/is/iclt/icenlp/core/icemorphy/IceMorphyResources.java
@@ -0,0 +1,26 @@
+package is.iclt.icenlp.core.icemorphy;
+
+import java.io.InputStream;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hrafn
+ * Date: 11/15/12
+ * Time: 5:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class IceMorphyResources {
+    final String dictPathTagger = "/dict/icetagger/";
+    public InputStream isDictionaryBase,isDictionary, isEndingsBase, isEndings, isEndingsProper, isPrefixes, isTagFrequency;
+
+    public IceMorphyResources() {
+        isDictionaryBase = getClass().getResourceAsStream( dictPathTagger + IceMorphyLexicons.dictionaryBase );
+        isDictionary = getClass().getResourceAsStream( dictPathTagger + IceMorphyLexicons.dictionary );
+        isEndingsBase = getClass().getResourceAsStream( dictPathTagger + IceMorphyLexicons.endingsBaseDictionary );
+        isEndings = getClass().getResourceAsStream( dictPathTagger + IceMorphyLexicons.endingsDictionary );
+        isEndingsProper = getClass().getResourceAsStream( dictPathTagger + IceMorphyLexicons.endingsProperDictionary );
+        isPrefixes = getClass().getResourceAsStream( dictPathTagger + IceMorphyLexicons.prefixesDictionary );
+        isTagFrequency = getClass().getResourceAsStream( dictPathTagger + IceMorphyLexicons.tagFrequencyFile );
+    }
+
+}
