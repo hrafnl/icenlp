204879d445a7bcb51f53b1aa52a4455972304a2f
Hrafn Loftsson <hrafn@kolibri.is>
2018-04-08 14:11:03 +0000

    Removes the cross-validation function from IceStagger and extracts the training code to a separate function

--- core/src/is/iclt/icenlp/runner/RunIceStagger.java
+++ core/src/is/iclt/icenlp/runner/RunIceStagger.java
@@ -53,44 +53,85 @@ public class RunIceStagger {
         return tagger;
     }
 
-    private static TaggedToken[][][] getSUCFold(
-            TaggedToken[][] sents, int fold)
-    {
-        TaggedToken[][][] parts = new TaggedToken[3][][];
-        HashSet<String> fileSet = new HashSet<String>();
-        for(TaggedToken[] sent : sents) {
-            String fileID = sent[0].id.substring(0,4);
-            fileSet.add(fileID);
+    private static void train(String trainFile,
+                              String devFile,
+                              String lexiconFile,
+                              String modelFile,
+                              String lang,
+                              int iceMorphyType,
+                              String fold,
+                              boolean extendLexicon,
+                              int posBeamSize,
+                              int neBeamSize,
+                              int maxPosIters,
+                              int maxNEIters,
+                              ArrayList<Dictionary> posDictionaries,
+                              ArrayList<Embedding> posEmbeddings,
+                              ArrayList<Dictionary> neDictionaries,
+                              ArrayList<Embedding> neEmbeddings) {
+        try {
+            TaggedToken[][] trainSents = null;
+            TaggedToken[][] devSents = null;
+            if(trainFile == null ||
+                    modelFile == null || lang == null)
+            {
+                System.err.println("Insufficient data.");
+                System.exit(1);
+            }
+            TaggedData td = new TaggedData(lang);
+            trainSents = td.readConll(
+                    trainFile, null, true, !trainFile.endsWith(".conll"));
+            if(devFile != null)
+                devSents = td.readConll(
+                        devFile, null, true, !devFile.endsWith(".conll"));
+            if(lang.equals("is") && devSents != null &&
+                    iceMorphyType > 0) {
+                Guesser.loadIceMorphy(fold);
+            }
+            System.err.println(
+                    "Read " + trainSents.length +
+                            " training sentences and " +
+                            ((devSents == null)? 0 : devSents.length) +
+                            " development sentences.");
+            Tagger tagger = getTagger(
+                    lang, td, posBeamSize, neBeamSize);
+            tagger.buildLexicons(trainSents);
+            Lexicon lexicon = tagger.getPosLexicon();
+            System.err.println("POS lexicon size (corpus): " +
+                    lexicon.size());
+            if(lexiconFile != null)
+            {
+                if(extendLexicon) {
+                    System.err.println(
+                            "Reading lexicon: " + lexiconFile);
+                } else {
+                    System.err.println(
+                            "Reading lexicon (not extending profiles): " +
+                                    lexiconFile);
+                }
+                lexicon.fromFile(lexiconFile, td.getPosTagSet(),
+                        extendLexicon);
+                System.err.println("POS lexicon size (external): " +
+                        lexicon.size());
+            }
+            tagger.setPosDictionaries(posDictionaries);
+            tagger.setPosEmbeddings(posEmbeddings);
+            tagger.setNEDictionaries(neDictionaries);
+            tagger.setNEEmbeddings(neEmbeddings);
+            tagger.setMaxPosIters(maxPosIters);
+            tagger.setMaxNEIters(maxNEIters);
+            if(lang.equals("is")) {
+                ((IceTagger)tagger).setIceMorphyType(iceMorphyType);
+            }
+            tagger.train(trainSents, devSents);
+            ObjectOutputStream writer = new ObjectOutputStream(
+                    new FileOutputStream(modelFile));
+            writer.writeObject(tagger);
+            writer.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.exit(1);
         }
-        ArrayList<String> files = new ArrayList<String>(fileSet);
-        Collections.sort(files);
-        assert files.size() == 500;
-        HashMap<String,Integer> fileNr = new HashMap<String,Integer>();
-        for(int i=0; i<files.size(); i++) {
-            String fileID = files.get(i);
-            fileNr.put(fileID, new Integer(i));
-        }
-        int nDev = 0, nTest = 0, nTrain = 0;
-        for(TaggedToken[] sent : sents) {
-            String fileID = sent[0].id.substring(0,4);
-            int nr = (int)fileNr.get(fileID);
-            if((nr % 10) == fold) nTest++;
-            else if((((nr+1) % 10) == fold) && (((nr / 10)) % 5 == 0)) nDev++;
-            else nTrain++;
-        }
-        parts[0] = new TaggedToken[nTrain][];
-        parts[1] = new TaggedToken[nDev][];
-        parts[2] = new TaggedToken[nTest][];
-        int iDev = 0, iTest = 0, iTrain = 0;
-        for(TaggedToken[] sent : sents) {
-            String fileID = sent[0].id.substring(0,4);
-            int nr = (int)fileNr.get(fileID);
-            if((nr % 10) == fold) parts[2][iTest++] = sent;
-            else if((((nr+1) % 10) == fold) && (((nr / 10)) % 5 == 0))
-                parts[1][iDev++] = sent;
-            else parts[0][iTrain++] = sent;
-        }
-        return parts;
     }
 
     public static void main(String[] args) {
@@ -106,7 +147,6 @@ public class RunIceStagger {
         int neBeamSize = 4;
         String lang = null;
         boolean preserve = false;
-        float embeddingSigma = 0.1f;
         boolean plainOutput = false;
         String fold = null;
         int maxPosIters = 16;
@@ -199,132 +239,25 @@ public class RunIceStagger {
                 devFile = args[++i];
             } else if(args[i].equals("-modelfile")) {
                 modelFile = args[++i];
-            } else if(args[i].equals("-train")) {
-                try {
-                    TaggedToken[][] trainSents = null;
-                    TaggedToken[][] devSents = null;
-                    if(trainFile == null ||
-                            modelFile == null || lang == null)
-                    {
-                        System.err.println("Insufficient data.");
-                        System.exit(1);
-                    }
-                    TaggedData td = new TaggedData(lang);
-                    trainSents = td.readConll(
-                            trainFile, null, true, !trainFile.endsWith(".conll"));
-                    if(devFile != null)
-                        devSents = td.readConll(
-                                devFile, null, true, !devFile.endsWith(".conll"));
-                    if(lang.equals("is") && devSents != null &&
-                            iceMorphyType > 0) {
-                        Guesser.loadIceMorphy(fold);
-                    }
-                    System.err.println(
-                            "Read " + trainSents.length +
-                                    " training sentences and " +
-                                    ((devSents == null)? 0 : devSents.length) +
-                                    " development sentences.");
-                    Tagger tagger = getTagger(
-                            lang, td, posBeamSize, neBeamSize);
-                    tagger.buildLexicons(trainSents);
-                    Lexicon lexicon = tagger.getPosLexicon();
-                    System.err.println("POS lexicon size (corpus): " +
-                            lexicon.size());
-                    if(lexiconFile != null)
-                    {
-                        if(extendLexicon) {
-                            System.err.println(
-                                    "Reading lexicon: " + lexiconFile);
-                        } else {
-                            System.err.println(
-                                    "Reading lexicon (not extending profiles): " +
-                                            lexiconFile);
-                        }
-                        lexicon.fromFile(lexiconFile, td.getPosTagSet(),
-                                extendLexicon);
-                        System.err.println("POS lexicon size (external): " +
-                                lexicon.size());
-                    }
-                    tagger.setPosDictionaries(posDictionaries);
-                    tagger.setPosEmbeddings(posEmbeddings);
-                    tagger.setNEDictionaries(neDictionaries);
-                    tagger.setNEEmbeddings(neEmbeddings);
-                    tagger.setMaxPosIters(maxPosIters);
-                    tagger.setMaxNEIters(maxNEIters);
-                    if(lang.equals("is")) {
-                        ((IceTagger)tagger).setIceMorphyType(iceMorphyType);
-                    }
-                    tagger.train(trainSents, devSents);
-                    ObjectOutputStream writer = new ObjectOutputStream(
-                            new FileOutputStream(modelFile));
-                    writer.writeObject(tagger);
-                    writer.close();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                    System.exit(1);
-                }
-            } else if(args[i].equals("-cross")) {
-                try {
-                    TaggedData td = new TaggedData(lang);
-                    TaggedToken[][] allSents =
-                            td.readConll(
-                                    trainFile, null, true,
-                                    !trainFile.endsWith(".conll"));
-                    Tagger tagger = getTagger(
-                            lang, td, posBeamSize, neBeamSize);
-                    tagger.setPosDictionaries(posDictionaries);
-                    tagger.setPosEmbeddings(posEmbeddings);
-                    tagger.setNEDictionaries(neDictionaries);
-                    tagger.setNEEmbeddings(neEmbeddings);
-                    final int nFolds = 10;
-                    /*
-                    int devPercent = 25;
-                    int testPercent = 25;
-                    */
-                    Evaluation eval = new Evaluation();
-                    for(int j=0; j<nFolds; j++) {
-                        Evaluation localEval = new Evaluation();
-                        TaggedToken[][][] parts = getSUCFold(allSents, j);
 
-                        System.err.println(
-                                "Fold " + j + ", train (" + parts[0].length +
-                                        "), dev (" + parts[1].length + "), test (" +
-                                        parts[2].length + ")");
-                        Lexicon lexicon = tagger.getPosLexicon();
-                        lexicon.clear();
-                        tagger.buildLexicons(parts[0]);
-                        if(lexiconFile != null)
-                            lexicon.fromFile(
-                                    lexiconFile, td.getPosTagSet(), extendLexicon);
-                        tagger.train(parts[0], parts[1]);
-                        for(TaggedToken[] sent : parts[2]) {
-                            TaggedToken[] taggedSent = tagger.tagSentence(
-                                    sent, true, false);
-                            eval.evaluate(taggedSent, sent);
-                            localEval.evaluate(taggedSent, sent);
-                            //tagger.getTaggedData().writeConllSentence(
-                            //    System.out, taggedSent);
-                            tagger.getTaggedData().writeConllGold(
-                                    System.out, taggedSent, sent, plainOutput);
-                        }
-                        System.err.println("Local POS accuracy: "+
-                                localEval.posAccuracy()+" ("+
-                                localEval.posCorrect+" / "+
-                                localEval.posTotal+")");
-                    }
-                    System.err.println("POS accuracy: "+eval.posAccuracy()+
-                            " ("+eval.posCorrect+" / "+
-                            eval.posTotal+")");
-                    System.err.println("NE precision: "+eval.nePrecision());
-                    System.err.println("NE recall:    "+eval.neRecall());
-                    System.err.println("NE F-score:   "+eval.neFscore());
-                    System.err.println("NE total:     "+eval.neTotal);
-                    System.err.println("NE correct:   "+eval.neCorrect);
-                    System.err.println("NE found:     "+eval.neFound);
-                } catch(Exception e) {
-                    e.printStackTrace();
-                    System.exit(1);
-                }
+            } else if(args[i].equals("-train")) {
+                train(  trainFile,
+                        devFile,
+                        lexiconFile,
+                        modelFile,
+                        lang,
+                        iceMorphyType,
+                        fold,
+                        extendLexicon,
+                        posBeamSize,
+                        neBeamSize,
+                        maxPosIters,
+                        maxNEIters,
+                        posDictionaries,
+                        posEmbeddings,
+                        neDictionaries,
+                        neEmbeddings);
+
             } else if(args[i].equals("-tag")) {
                 try {
                     if(modelFile == null || i >= args.length-1) {
