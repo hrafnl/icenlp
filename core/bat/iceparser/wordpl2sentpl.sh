5cfe6d9154e046b245e74695f48b5f52d87eeb2a
Hrafn Loftsson <hrafnl@gmail.com>
2021-03-13 10:43:04 +0000

    Adds a parameter to shell script

--- core/bat/iceparser/wordpl2sentpl.sh
+++ core/bat/iceparser/wordpl2sentpl.sh
@@ -1,7 +1,7 @@
-# Usage: ./wordpl2sentpl.sh <file>
+# Usage: ./wordpl2sentpl.sh <input file> <output file>
 # Converts 'word/tag per line' to 'tagged sentence per line'
 
 # If the following sed command does not work then use the awk command below
-#sed -z 's/\(\S\+\)\n/\1\t/g' $1 | sed 's/\t/ /g' > $1".txt"
+#sed -z 's/\(\S\+\)\n/\1\t/g' $1 | sed 's/\t/ /g' > $2
 
-awk '{if (NF != 0) {sent = sent " " $0} else {print sent; sent = ""}}' $1 | sed 's/\t/ /g' | sed 's/^ //'> $1.txt
+awk '{if (NF != 0) {sent = sent " " $0} else {print sent; sent = ""}}' $1 | sed 's/\t/ /g' | sed 's/^ //'> $2
